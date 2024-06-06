0764ec230d50120f4cd50a3e2921c44ca5dde664
Benjamin Maier <maier.bn@gmail.com>
2024-06-06 18:49:53 +0200

    update fetch_events

--- fetch_events.sh
+++ fetch_events.sh
@@ -4,3 +4,6 @@ one_year_from_now=$(date -d "+1 year" +%Y-%m-%d)
 
 # fetch events from churchtools and store in "events.json"
 wget -O events.json "https://elkw2808.krz.tools/api/calendars/31/appointments?from=${current_date}&to=${one_year_from_now}"
+
+echo "finished downloading events at ${current_date}" > msg.html
+
