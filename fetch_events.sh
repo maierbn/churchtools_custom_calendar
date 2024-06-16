cbcee21ba0ca0ba5a9fe61586cf465cb7b2586d5
Benjamin Maier <benjamin.maier2@de.bosch.com>
2024-06-16 16:23:06 +0200

    append to array

--- fetch_events.sh
+++ fetch_events.sh
@@ -9,12 +9,20 @@ data="func=getMasterData"
 # Making the POST request using curl
 response=$(curl -s -X POST -d "$data" "$url")
 
-# Printing the response
-echo "Response from server:"
-echo "$response"
+# Extracting the list of integers using jq
+calendar_categories=$(echo "$response" | jq -r '.data.category | keys[] | tonumber')
 
-# fetch events from churchtools and store in "events.json"
-wget -O events.json "https://elkw2808.krz.tools/api/calendars/31/appointments?from=${current_date}&to=${one_year_from_now}"
+# Initialize the events.json file
+echo "[]" > events.json
+
+# Iterate over each integer and fetch the JSON results
+for category_id in $calendar_categories; do
+    api_url="https://elkw2808.krz.tools/api/calendars/$category_id/appointments?from=${current_date}&to=${one_year_from_now}"
+    appointment_response=$(curl -s "$api_url")
+    
+    # Append the fetched JSON to the events.json file
+    jq ". += [$appointment_response]" events.json > temp.json && mv temp.json events.json
+done
 
 # available calendars: 31, 49, 52, 55, 58, 60, 67, 73, 76
 
