471ff6e8a14dbf95d1b5a35a9044a76a6d9905b8
Benjamin Maier <benjamin.maier2@de.bosch.com>
2024-06-16 15:46:35 +0200

    post to api to get available calendars

--- fetch_events.sh
+++ fetch_events.sh
@@ -2,8 +2,21 @@
 current_date=$(date +%Y-%m-%d)
 one_year_from_now=$(date -d "+1 year" +%Y-%m-%d)
 
+# query the available calendars
+url="https://elkw2808.krz.tools/index.php?q=churchcal%2Fajax"
+data="func=getMasterData"
+
+# Making the POST request using curl
+response=$(curl -s -X POST -d "$data" "$url")
+
+# Printing the response
+echo "Response from server:"
+echo "$response"
+
 # fetch events from churchtools and store in "events.json"
 wget -O events.json "https://elkw2808.krz.tools/api/calendars/31/appointments?from=${current_date}&to=${one_year_from_now}"
+
+# available calendars: 31, 49, 52, 55, 58, 60, 67, 73, 76
 
 echo "finished downloading events at ${current_date}" > msg.html
 ls -l >> msg.html
